e3c1342588de0e8cdf2c7fa686bf67e83c6a4de3
Tero Laxström <tlax@neonpeons.com>
2025-05-20 18:00:07 +0300

    Adding example

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # potential-octo-rotary-phone
+
+Note: PLZ don't use :D
+
+Very simple test "framework", if you can even call it that.
+
+## Example
+
+Running `lua example.lua`
+```
+❯ lua example.lua 
+Failures:
+  1: summing 1 + 2 equals 4
+    3 != 4
+
+Failed tests: 1/2
+```
+
---
+++ example.lua
@@ -0,0 +1,17 @@
+-- pretended functionality
+function add(a, b)
+  return a + b
+end
+
+-- testing example
+local T = require("test")
+
+T.it('summing 1 + 2 equals 3', function()
+  return T.expect(add(1, 2), 3)
+end)
+
+T.it('summing 1 + 2 equals 4', function()
+  return T.expect(add(1, 2), 4)
+end)
+
+T.run()
